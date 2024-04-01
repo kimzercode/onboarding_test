5cb0a12e1c1e11938b3c1d57890c371ec23e53c3
kimzercode <84059452+kimzercode@users.noreply.github.com>
2024-04-01 13:35:34 +0000

    fixed error no views named index
    views file in training app contained no functions

--- training/views.py
+++ training/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import User, Activity, UserActivityLog, do_training
 
 # Create your views here.
+def index(request):
+    activity_list = Activity.objects.all()
+    users_list = User.objects.filter(is_staff=False)
+    users=[]
+    for user in users_list:
+        user_activity = UserActivityLog.objects.filter(user_activity__user__username=user.username)
+        score = 0
+        for activity in user_activity:
+            score += activity.score
+        userProfile={}
+        userProfile["username"] = user.username
+        userProfile["score"] = score
+        users.append(userProfile)
+    users.append({"username": "Dyonisia", "score": 640})
+    users = sorted(users, key=lambda x: x["score"], reverse=True)
+    context = {"activity_list": activity_list, "users": users}
+    return render(request, "training/index2.html", context)
+
+def train(request):
+    training_score = do_training
+    return render(request, "training/train.html", {"score": training_score})
